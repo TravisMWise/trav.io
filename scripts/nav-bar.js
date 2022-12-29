33a64b22deeaa43a216c35823d1b2f40dd4cb270
Travis Wise <86330962+TravisMWise@users.noreply.github.com>
2022-12-29 18:36:09 -0500

    Comment out about page and add in stargazers to games section

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,18 +1,60 @@
-document.getElementById("logo-name").addEventListener("click", () => {
-    window.open('index.html', "_self");
-});
-document.getElementById("home").addEventListener("click", () => {
-    window.open('index.html', "_self");
-});
-document.getElementById("about").addEventListener("click", () => {
-    window.open('about.html', "_self");
-});
-document.getElementById("projects").addEventListener("click", () => {
-    window.open('projects.html', "_self");
-});
-document.getElementById("contact").addEventListener("click", () => {
-    window.open('contact.html', "_self");
-});
-document.getElementById("games").addEventListener("click", () => {
-    window.open('games.html', "_self");
-});
+if (document.getElementById("logo-name")) {
+    document.getElementById("logo-name").addEventListener("click", () => {
+        window.open('index.html', "_self");
+    });
+}
+if (document.getElementById("home")) {
+    document.getElementById("home").addEventListener("click", () => {
+        window.open('index.html', "_self");
+    });
+}
+if (document.getElementById("about")) {
+    document.getElementById("about").addEventListener("click", () => {
+        window.open('about.html', "_self");
+    });
+}
+if (document.getElementById("projects")) {
+    document.getElementById("projects").addEventListener("click", () => {
+        window.open('projects.html', "_self");
+    });
+}
+if (document.getElementById("contact")) {
+    document.getElementById("contact").addEventListener("click", () => {
+        window.open('contact.html', "_self");
+    });
+}
+if (document.getElementById("games")) {
+    document.getElementById("games").addEventListener("click", () => {
+        window.open('games.html', "_self");
+    });
+}
+if (document.getElementById("games-logo-name")) {
+    document.getElementById("games-logo-name").addEventListener("click", () => {
+        window.open('../../index.html', "_self");
+    });
+}
+if (document.getElementById("games-home")) {
+    document.getElementById("games-home").addEventListener("click", () => {
+        window.open('../../index.html', "_self");
+    });
+}
+if (document.getElementById("games-about")) {
+    document.getElementById("games-about").addEventListener("click", () => {
+        window.open('../../about.html', "_self");
+    });
+}
+if (document.getElementById("games-projects")) {
+    document.getElementById("games-projects").addEventListener("click", () => {
+        window.open('../../projects.html', "_self");
+    });
+}
+if (document.getElementById("games-contact")) {
+    document.getElementById("games-contact").addEventListener("click", () => {
+        window.open('../../contact.html', "_self");
+    });
+}
+if (document.getElementById("games-games")) {
+    document.getElementById("games-games").addEventListener("click", () => {
+        window.open('../../games.html', "_self");
+    });
+}

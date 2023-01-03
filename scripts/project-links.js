711f9948d1a3c20b9532a137351aaad26ed5c043
Travis Wise <86330962+TravisMWise@users.noreply.github.com>
2023-01-03 15:13:49 -0500

    Remove simple projects from projects page and add dimension

--- scripts/project-links.js
+++ scripts/project-links.js
@@ -1,41 +1,60 @@
 // Textio
-document.getElementById('textio').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/textio");
-});
-document.getElementById('textioImg').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/textio");
-});
+if (document.getElementById('textio')) {
+    document.getElementById('textio').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/textio");
+    });
+    document.getElementById('textioImg').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/textio");
+    });
+}
+
 
 // Unreal Engine 4
-document.getElementById('ue4').addEventListener("click", () => {
-    window.open("https://drive.google.com/drive/folders/1DPBOyVJXo4un7DFItsDdtRF2Hx21PTXS?usp=sharing");
-});
-document.getElementById('ue4Img').addEventListener("click", () => {
-    window.open("https://drive.google.com/drive/folders/1DPBOyVJXo4un7DFItsDdtRF2Hx21PTXS?usp=sharing");
-});
+if (document.getElementById('ue4')) {
+    document.getElementById('ue4').addEventListener("click", () => {
+        window.open("https://drive.google.com/drive/folders/1DPBOyVJXo4un7DFItsDdtRF2Hx21PTXS?usp=sharing");
+    });
+    document.getElementById('ue4Img').addEventListener("click", () => {
+        window.open("https://drive.google.com/drive/folders/1DPBOyVJXo4un7DFItsDdtRF2Hx21PTXS?usp=sharing");
+    });
+}
 
 // Stargazers
-document.getElementById('stargazer').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/stargazers");
-});
-document.getElementById('stargazerImg').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/stargazers");
-});
+if (document.getElementById('stargazer')) {
+    document.getElementById('stargazer').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/stargazers");
+    });
+    document.getElementById('stargazerImg').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/stargazers");
+    });
+}
 
 // MERN Stack
-document.getElementById('mern').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/diary-applicaiton");
-});
-document.getElementById('mernImg')
-    .addEventListener("click", () => {
+if (document.getElementById('mern')) {
+    document.getElementById('mern').addEventListener("click", () => {
         window.open("https://github.com/TravisMWise/diary-applicaiton");
     });
+    document.getElementById('mernImg').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/diary-applicaiton");
+    });
+}
 
-// PHP
-document.getElementById('ceo').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/ClubEventOrganizer");
-});
-document.getElementById('ceoImg').addEventListener("click", () => {
-    window.open("https://github.com/TravisMWise/ClubEventOrganizer");
-});
+// ClubEventOrganizer
+if (document.getElementById('ceo')) {
+    document.getElementById('ceo').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/ClubEventOrganizer");
+    });
+    document.getElementById('ceoImg').addEventListener("click", () => {
+        window.open("https://github.com/TravisMWise/ClubEventOrganizer");
+    });
+}
 
+// Dimension
+if (document.getElementById('dimension')) {
+    document.getElementById('dimension').addEventListener("click", () => {
+        window.open("http://travtutor.com/games/dimension/index.html");
+    });
+    document.getElementById('dimensionImg').addEventListener("click", () => {
+        window.open("http://travtutor.com/games/dimension/index.html");
+    });
+}
